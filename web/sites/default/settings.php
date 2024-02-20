056f1656c41f533baa7b9c6f6f5c99659174dffd
MichelleDuque <michelled_1994@hotmail.com>
2024-02-19 22:57:30 -0500

    second config

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -32,3 +32,7 @@ $local_settings = __DIR__ . "/settings.local.php";
 if (file_exists($local_settings)) {
   include $local_settings;
 }
+
+$settings['config_sync_directory'] = '../config/sync';
+
+$settings['hash_salt'] = 'your_random_salt_value_here';
